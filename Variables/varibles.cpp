af7e59c6eb6fb845b5b69bbbc5be0c5e8da5e6cd
joymusila <neemajoy476@gmail.com>
2024-02-20 17:48:13 +0300

    variables in c++

---
+++ Variables/varibles.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+int age = 23;
+
+double height = 1.74;
+
+string name = "Paul";
+
+cout << "My name is " << name << ", and I am " << age << " years old." << endl;
+
+cout << "I am " << height << " meters tall." << endl;
+
+return 0;
+
+}
+
+
